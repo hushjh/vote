75bd1623fe5225d5b924e3eb9bb040a4840c98b4
hushjh <772074834@qq.com>
2020-07-07 11:41:15 +0800

    new tag dev20200607114115 published by hushjh

--- auto/tag/dev.js
+++ auto/tag/dev.js
@@ -26,7 +26,7 @@ async function init() {
   console.log("curBranch:", curBranch);
   let tag = getTagStr();
   console.log("tag:", tag);
-  let cmdStr = `git add . && git commit -m "new tag dev$tag published by ${userName}" --no-verify && git push`
+  let cmdStr = `git add . && git commit -m "new tag ${tag} published by ${userName}" --no-verify && git push`
   let et = await execFun(cmdStr);
   console.log("et:", et)
 }
